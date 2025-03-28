f608c14bd23df481e9a1e92fff2d7e9b889eb5ae
Piwoksn <piwoksn@gmail.com>
2025-03-28 21:06:42 +0100

    imported requests for api and datetime, also added api url

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render, HttpResponse
+import requests
+import datetime
 
 # Create your views here.
 def home(request):
+    url: f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid=91a016a5ded7f0fd7f29ebb4d50c8090'
     return render(request, 'mainapp/index.html')
