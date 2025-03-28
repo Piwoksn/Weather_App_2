a4ad6c6ac9fa9dd56a2e5a6404a0b6c15b87bf2d
Piwoksn <piwoksn@gmail.com>
2025-03-28 21:18:06 +0100

    concluded views.home and api setup

--- mainapp/views.py
+++ mainapp/views.py
@@ -4,5 +4,26 @@ import datetime
 
 # Create your views here.
 def home(request):
+    if 'city' in request.POST:
+        city = request.POST['city']
+    else:
+        city = "Port Harcourt"
     url: f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid=91a016a5ded7f0fd7f29ebb4d50c8090'
-    return render(request, 'mainapp/index.html')
+    PARAMS = {'units':'metrics'}
+    
+    data = request.get(url, PARAMS).json()
+
+    description = data['weather'][0]['description']
+    icon = data['weather'][0]['icon']
+    temp = data['main'][0]['temp']
+    
+    day = datetime.date.today()
+    
+    context = {
+        'description': description,
+        'icon': icon,
+        'temp': temp,
+        'day': day,
+    }
+    
+    return render(request, 'mainapp/index.html', context)
