9bd561a029aa38b84395ba77fda47f8ff94391ba
Piwoksn <piwoksn@gmail.com>
2025-03-29 23:05:02 +0100

    fixed the home function in views.py

--- mainapp/views.py
+++ mainapp/views.py
@@ -7,23 +7,29 @@ def home(request):
     if 'city' in request.POST:
         city = request.POST['city']
     else:
-        city = "Port Harcourt"
-    url: f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid=91a016a5ded7f0fd7f29ebb4d50c8090'
-    PARAMS = {'units':'metrics'}
+        city = "lagos"
     
-    data = request.get(url, PARAMS).json()
+    url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid=91a016a5ded7f0fd7f29ebb4d50c8090'
+    
+    PARAMS = {'units': 'metric'}  
+    
+    response = requests.get(url, params=PARAMS)
+    data = response.json()
 
-    description = data['weather'][0]['description']
-    icon = data['weather'][0]['icon']
-    temp = data['main'][0]['temp']
-    
-    day = datetime.date.today()
-    
-    context = {
-        'description': description,
-        'icon': icon,
-        'temp': temp,
-        'day': day,
-    }
-    
+    if response.status_code == 200:  
+        description = data['weather'][0]['description']
+        icon = data['weather'][0]['icon']
+        temp = data['main']['temp']
+
+        day = datetime.date.today()
+
+        context = {
+            'description': description,
+            'icon': icon,
+            'temp': temp,
+            'day': day,
+        }
+    else:
+        context = {'error': 'City not found. Please try again.'}
+
     return render(request, 'mainapp/index.html', context)
