4715320a4bc0c3b16cc6b3f1fdc3eabe1a2b29de
Piwoksn <piwoksn@gmail.com>
2025-03-31 13:43:19 +0100

    updated views

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
 import requests
 import datetime
 
@@ -9,44 +9,51 @@ def home(request):
     else:
         city = "lagos"
     
-    # City url (Google search engine)
-    api_key ="AIzaSyCkgEmVtuqJ1Dqzj_qvUu5s2-DIuJOuSXY"
-    cse_id = "90ee7e8f4659d4b21"
-    query = f"{city} 1920x1080"
-    page = 1
-    start = (page-1) * 10 + 1
-    searchType = 'image'
-    city_url = f"https://www.googleapis.com/customsearch/v1?q={query}&cx={cse_id}&key={api_key}&start={start}&searchType={searchType}"
-    city_data = requests.get(city_url).json()
-    search_items = city_data.get("items")
-    image_url = search_items[2]['link']
-    
-    print(image_url)
-    
-    # weather -----
-    url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid=91a016a5ded7f0fd7f29ebb4d50c8090'
-    
-    PARAMS = {'units': 'metric'}  
+    try:
+        # City url (Google search engine)
+        api_key ="AIzaSyCkgEmVtuqJ1Dqzj_qvUu5s2-DIuJOuSXY"
+        cse_id = "90ee7e8f4659d4b21"
+        query = f"{city} 1920x1080"
+        page = 1
+        start = (page-1) * 10 + 1
+        searchType = 'image'
+        city_url = f"https://www.googleapis.com/customsearch/v1?q={query}&cx={cse_id}&key={api_key}&start={start}&searchType={searchType}"
+        city_data = requests.get(city_url).json()
+        search_items = city_data.get("items")
+        image_url = search_items[1]['link']
+        
+        
+        # weather -----
+        url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid=91a016a5ded7f0fd7f29ebb4d50c8090'
+        
+        PARAMS = {'units': 'metric'}  
 
-    response = requests.get(url, params=PARAMS)
-    data = response.json()
+        response = requests.get(url, params=PARAMS)
+        data = response.json()
 
-    if response.status_code == 200:  
-        description = data['weather'][0]['description']
-        icon = data['weather'][0]['icon']
-        temp = data['main']['temp']
+        if response.status_code == 200:  
+            description = data['weather'][0]['description']
+            icon = data['weather'][0]['icon']
+            temp = data['main']['temp']
 
-        day = datetime.date.today()
+            day = datetime.date.today()
 
+            context = {
+                'description': description,
+                'icon': icon,
+                'temp': temp,
+                'day': day,
+                'city': city, 
+                'image':image_url,
+            }
+        else:
+            context = {'error': 'City not found. Please try again.'}
+
+    except:
+        message = "check network connection !"
         context = {
-            'description': description,
-            'icon': icon,
-            'temp': temp,
-            'day': day,
-            'city': city, 
-            'image':image_url,
+            'message': message, 
         }
-    else:
-        context = {'error': 'City not found. Please try again.'}
+        return render(request, 'mainapp/index.html', context)
 
     return render(request, 'mainapp/index.html', context)
